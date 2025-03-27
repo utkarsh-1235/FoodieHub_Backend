f6298de18941cb9704a80bc062d68340d1b22c24
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-03-27 22:17:23 +0530

    PaymentController.js and OrderSchema.js

--- Controller/PaymentController.js
+++ Controller/PaymentController.js
@@ -1,10 +1,48 @@
 const paymentModel = require('../Models/PaymentSchema');
+const Razorpay = require('razorpay');
+const userModel = require('../Models/userSchema');
+const orderModel = require('../Models/OrderSchema');
+
+const razorpay = new Razorpay({
+    key_id: process.env.KEY_ID,
+    key_secret: process.env.KEY_SECRET
+})
 
 const createPayment = async(req, res)=>{
     try{
+  const {userId, amount, paymentSignature, paymentMethod} = req.body;
+
+  if(!userId || !amount || !paymentSignature || !paymentMethod){
+    res.status(400).json('All the fields required');
+  }
+
+  const user = await userModel.findById(userId);
+  if(!user){
+    return res.status(401).json('User not exist');
+  }
+  const payment = new paymentModel({
+     user: {
+        name: user. name,
+        email: user.email
+     },
+     paymentId: paymentSignature,
+     amount: amount,
+     paymentMethod: paymentMethod
+
+  })
+  await payment.save();
+
+  res.status(200).json({
+    success: true,
+    payment
+  })
 
     }catch(err){
-
+        console.log(err);
+   res.status(500).json({
+     status: false,
+     message: err.message
+   })
     }
 }
 
--- Models/OrderSchema.js
+++ Models/OrderSchema.js
@@ -2,7 +2,75 @@ const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
 const orderSchema = new Schema({
-
+  user: {
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    }
+   
+  },
+  items: [
+    {
+        dish:{
+            dishId: {
+                type: Schema.Types.ObjectId,
+                ref: 'Dish',
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+  ],
+  totalPrice: {
+     type: Number,
+     required: true
+  },
+  totalItems: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Confirmed', 'Preparing', 'Out for Delivery', 'Delivered', 'Cancelled'],
+    default: 'Pending'
+  },
+  paymentStatus: {
+    type: String,
+    enum: ['Pending', 'Paid', 'Failed'],
+    default: 'Pending'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
 },{
     timestamps: true
 })
